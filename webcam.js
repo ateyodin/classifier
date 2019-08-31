bb185f8b547b5ac20420110d620fea2d5dcd0351
ateyodin <52425179+ateyodin@users.noreply.github.com>
2019-08-31 12:08:14 -0400

    Update webcam.js

--- webcam.js
+++ webcam.js
@@ -1,7 +1,7 @@
+let net;
+
 const classifier = knnClassifier.create();
 const webcamElement = document.getElementById('webcam');
-
-let net;
 
 async function app() {
   console.log('Loading mobilenet..');
